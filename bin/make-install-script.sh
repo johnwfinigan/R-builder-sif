91ef675e49193ae92f869bf635bcfa4af6d37e59
John Finigan <John>
2021-06-03 17:11:41 -0600

    bioconductor noncurrent version take 2

--- bin/make-install-script.sh
+++ bin/make-install-script.sh
@@ -9,6 +9,7 @@ t1=$(mktemp)
 t2=$(mktemp)
 t3=$(mktemp)
 t="$t2"
+:> "$1/tmp/R-packages.sh"
 
 cat packages-cran.txt > "$packagelist"
 # if we're going to be installing from Bioconductor,
@@ -29,7 +30,7 @@ printf '), repos="https://cloud.r-project.org/", Ncpus=8)@\n' >> "$t2"
 
 
 if [ -f packages-bioc.txt ] ; then
-  printf 'R --slave -e @BiocManager::install(version = "%s")@\n' "$biocversion" >> "$t2"
+  printf 'R --slave -e @BiocManager::install(version = "%s", ask = FALSE, force = TRUE)@\n' "$biocversion" >> "$t2"
   printf 'R --slave -e @BiocManager::install(c(' >> "$t2"
   grep -v '^#' packages-bioc.txt | while read -r p ; do
     printf '"%s", ' "$p" >> "$t2"
