2bbbde1620327948bf25c652ecd52cda70b65733
John Finigan <john@Johns-Mac-mini.local>
2022-03-22 14:07:30 -0600

    improve test console logging, gitignore

--- test/test.sh
+++ test/test.sh
@@ -3,7 +3,7 @@
 set -eu
 getname() {
   rand=$(dd if=/dev/urandom count=1 bs=512 2>/dev/null | openssl sha1 | awk '{print $NF}')
-  randname="r-builder-sif-temporary-${rand}"
+  randname="r-builder-sif-test-temporary-${rand}"
 }
 
 echo "$(date) starting tests" > test.log
@@ -13,14 +13,14 @@ echo data.table > packages-cran.txt
 getname
 #./make-container.sh -n "$randname"
 ./make-container.sh "$randname"
-echo "$(date) Basic case pass" >> test.log
+echo "$(date) Basic case pass" | tee -a test.log
 
 # bioconductor with defaults
 :> packages-cran.txt
 echo IRanges > packages-bioc.txt
 getname
 ./make-container.sh "$randname"
-echo "$(date) Bioconductor case pass" >> test.log
+echo "$(date) Bioconductor case pass" | tee -a test.log
 
 # test use of post script
 :> packages-cran.txt
@@ -28,7 +28,7 @@ echo IRanges > packages-bioc.txt
 echo true > post.txt
 getname
 ./make-container.sh -p post.txt "$randname"
-echo "$(date) post script case pass" >> test.log
+echo "$(date) post script case pass" | tee -a test.log
 
 # custom R version, guess bioconductor version, test sif generation
 echo IRanges > packages-bioc.txt
@@ -39,7 +39,7 @@ if [ ! -f "${randname}.sif" ] ; then
   echo "sif file was not generated, exiting" >&2
   exit 150
 fi
-echo "$(date) custom R case pass, sif pass" >> test.log
+echo "$(date) custom R case pass, sif pass" | tee -a test.log
 
 # test convert without tag
 docker pull ubuntu:latest
@@ -49,7 +49,7 @@ if [ ! -f ubuntu.sif ] ; then
   echo "convert only test failed" >&2
   exit 151
 fi
-echo "$(date) untagged convert pass" >> test.log
+echo "$(date) untagged convert pass" | tee -a test.log
 
 # test convert with tag
 # centos:7 will have already been pulled by sif generation step
@@ -59,9 +59,9 @@ if [ ! -f centos_7.sif ] ; then
   echo "convert only test failed" >&2
   exit 151
 fi
-echo "$(date) tagged convert pass" >> test.log
+echo "$(date) tagged convert pass" | tee -a test.log
 
-echo "$(date) tests completed, elapsed $SECONDS seconds" >> test.log
+echo "$(date) tests completed, elapsed $SECONDS seconds" | tee -a test.log
 
 
 
